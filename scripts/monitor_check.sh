343e44a6aaffd025aead30984d4bd1f6db7eacb8
Liadov Aleksandr <aleksandr.liadov.ext@parrot.com>
2021-03-16 09:21:38 +0100

    [DEV] Auto monitor check, dropbox mount

--- scripts/monitor_check.sh
+++ scripts/monitor_check.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-
-# First argument: Primary monitor
-# Second argument: Secondary monitor
 if  xrandr | grep "DP-4 c"; then
     xrandr --output DP-4 --auto --primary
+    xrandr --output DP-2 --on
     xrandr --output DP-2 --right-of DP-4
     xrandr --output DP-3 --off
     killall plasmashell
@@ -13,4 +11,5 @@ else
     killall plasmashell
     kstart plasmashell
 fi
+# if smth wrong with nvidia xserver "service sddm restart"
 #sh ~/.fehbg # wallpaper can look weird if not refreshed
